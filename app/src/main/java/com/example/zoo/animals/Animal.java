422d4c403e1e11d520ec290be45eb9a642b81a1e
Rob <robdwilliams85@gmail.com>
2017-11-10 11:42:14 +0000

    Created Animal class

---
+++ app/src/main/java/com/example/zoo/animals/Animal.java
@@ -0,0 +1,17 @@
+package com.example.zoo.animals;
+
+/**
+ * Created by user on 10/11/2017.
+ */
+
+public class Animal {
+    double cashValue;
+    String name;
+    String type;
+
+    public Animal(double cashValue, String name, String type) {
+        this.cashValue= cashValue;
+        this.name = name;
+        this.type = type;
+    }
+}
